286b7f9e1590dd2637edd9cc1e89dd9fe7aa9017
Hoàng Nguyên <153731374+HoangNguyennnnnnn@users.noreply.github.com>
2024-08-31 20:34:26 +0700

    Add files via upload

---
+++ web_mysqli/pages/main/index.php
@@ -0,0 +1,35 @@
+<h3>Sản phẩm mới nhất</h3>
+                <ul class="product-list">
+                    <li>
+                        <a href="">
+                            <img src="images/tai-nghe-sony-001.jpg" alt="">
+                            <p class="title-product">Tên sản phẩm: Tai nghe Sony 001</p>
+                            <p class="title-price">Giá : 300000 vnd</p>
+                        </a>
+                    </li>
+                    <li>
+                        <img src="images/tai_nghe_sony_transparent.jpg" alt="">
+                        <p>Tên sản phẩm: Tai nghe Sony 001</p>
+                        <p>Giá : 300000 vnd</p>
+                    </li>
+                    <li>
+                        <img src="images/tai-nghe-sony-001.jpg" alt="">
+                        <p>Tên sản phẩm: Tai nghe Sony 001</p>
+                        <p>Giá : 300000 vnd</p>
+                    </li>
+                    <li>
+                        <img src="images/tai-nghe-sony-001.jpg" alt="">
+                        <p>Tên sản phẩm: Tai nghe Sony 001</p>
+                        <p>Giá : 300000 vnd</p>
+                    </li>
+                    <li>
+                        <img src="images/tai-nghe-sony-001.jpg" alt="">
+                        <p>Tên sản phẩm: Tai nghe Sony 001</p>
+                        <p>Giá : 300000 vnd</p>
+                    </li>
+                    <li>
+                        <img src="images/tai-nghe-sony-001.jpg" alt="">
+                        <p>Tên sản phẩm: Tai nghe Sony 001</p>
+                        <p>Giá : 300000 vnd</p>
+                    </li>
+                </ul>
---
+++ web_mysqli/pages/menu.php
@@ -0,0 +1,9 @@
+<div class="menu">
+            <ul class="list-menu">
+                <li><a href="index.php">Trang chu</a></li>
+                <li><a href="index.php?quanly=danhmucsanpham&id=1">Danh muc san pham</a></li>
+                <li><a href="index.php?quanly=giohang">Gio hang</a></li>
+                <li><a href="index.php?quanly=tintuc">Tin tuc</a></li>
+                <li><a href="index.php?quanly=lienhe">Lien he</a></li>
+            </ul>
+        </div>
